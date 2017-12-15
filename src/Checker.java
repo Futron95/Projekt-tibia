a64f4c21a6a21bc362a2929621662109e1513030
Mateusz <matfut95@gmail.com>
2017-12-15 18:52:20 +0100

    Dodanie strong healowania sie, poprawienie komentarzy.

--- src/Checker.java
+++ src/Checker.java
@@ -1,14 +1,16 @@
 import java.awt.*;
 
 public class Checker {
-    public boolean running;             //true oznacza ze leczenie jest wlaczone
-    public static boolean bars = true;   //wskazuje czy paski zdrowia i many sa w odpowiednim miejscu (na podstawie konkretnego piksela z serduszka kolo paska hp)
+
+    private static Color barsColor = new Color(219,79,79);  //kolor serduszka używany przy określeniu czy paski many i hp są tak gdzie powinny
+    private static boolean bars = true;   //wskazuje czy paski zdrowia i many sa w odpowiednim miejscu (na podstawie konkretnego piksela z serduszka kolo paska hp)
+
+    public boolean running;             //true oznacza ze reagowanie jest wlaczone
     public Pixel pixel;                  //pixel do sprawdzenia przy orzekaniu czy dokonać akcji(leczenie albo picie many)
     private int keyCode;                //kod klawisza do wcisniecia w celu wywolania akcji, np. F1 to KeyEvent.VK_F1
     private long lastAction = 0;        //przechowuje moment, w którym ostatni raz wciśnięto klawisz akcji
-    private static Color barsColor = new Color(219,79,79);  //kolor serduszka używany przy określeniu czy paski many i hp są tak gdzie powinny
 
-    public static boolean checkBars()
+    public static boolean checkBars()       //metoda statyczna sprawdzająca czy paski hp i many są w przewidywanym miejscu
     {
         boolean newBars = Main.robot.getPixelColor((int)Main.screenSize.getWidth()-160, 33).equals(barsColor);    //sprawdzanie czy paski hp i many są w odpowiednim miejscu
         if (bars != newBars) {
@@ -21,6 +23,13 @@ public class Checker {
         return newBars;
     }
 
+    public Checker(boolean running, Pixel pixel, int keyCode)   //konstruktor
+    {
+        this.running = running;
+        this.pixel = pixel;
+        this.keyCode = keyCode;
+    }
+
     private boolean checkColor()        //metoda sprawdzajaca warunek, ze pixel jest odpowiedniego koloru do wykonania akcji (dla healingu czerwien pixela musi byc mniejsza niz 150 zeby stwierdzic ze pasek hp nie jest zapelniony w danym miejscu)
     {
         Pixel checkedPixel = Pixel.getPixel(pixel.x, pixel.y);
@@ -31,13 +40,14 @@ public class Checker {
         return true;
     }
 
-    public void check()
+    public void check()             //metoda wciskająca programowo odpowiedni przycisk, gdy zostaną spełnione wszystkie niezbędne warunki
     {
-        long newAction = System.currentTimeMillis();
-        if(!checkBars())
+        if(!checkBars())                //czy paski hp i many na przewidywanym miejscu?
             return;
-        if (running && newAction-lastAction>1000 && checkColor()) { //jezeli wartosc R piksela na pasku zdrowia jest mniejsza niz 150 znaczy ze postac nie ma 100% hp
-            Main.robot.keyPress(keyCode);                                //programowe wcisniecie F1
+        long newAction = System.currentTimeMillis();
+        if (running && newAction-lastAction>1000 && checkColor())   //jezeli reagowanie jest wlaczone, ostatnia akcja była wiecej niż 1000ms temu a sprawdzany kolor się zgadza
+        {
+            Main.robot.keyPress(keyCode);                                //programowe wcisniecie klawisza akcji (np. F1 dla healingu)
             lastAction = System.currentTimeMillis();
             try {
                 Thread.sleep(Main.r.nextInt(40)+80);        //losowe opoznienie (80-120ms) aby zasymulowac czynnik ludzki
@@ -47,10 +57,4 @@ public class Checker {
         }
     }
 
-    public Checker(boolean running, Pixel pixel, int keyCode)
-    {
-        this.running = running;
-        this.pixel = pixel;
-        this.keyCode = keyCode;
-    }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,24 +8,29 @@ public class Main
     public static Robot robot;
     public static Random r;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception
+    {
         screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         System.out.println("Wymiary ekranu: szerokosc "+screenSize.getWidth()+", wysokość "+screenSize.getHeight());
         robot = new Robot();
         r = new Random();
+        Checker strongHealing = new Checker(true, new Pixel(150, 0, 0,(int)Main.screenSize.getWidth()-120, 33), KeyEvent.VK_F4);
         Checker healing = new Checker(true, new Pixel(150,0,0, (int)Main.screenSize.getWidth()-70, 33), KeyEvent.VK_F1);    //obiekt sluzacy do sprawdzania hp, jezeli pasek hp konczy sie wczesniej niz 70 pixeli od konca ekranu, wywolywana jest akcja
         Checker manaing = new Checker(true, new Pixel(0,0,150, (int)Main.screenSize.getWidth()-120, 46), KeyEvent.VK_F2);
         Thread checking = new Thread(() ->
         {
             while(true)
             {
+                strongHealing.check();
                 healing.check();
                 manaing.check();
+
                 try {
                     Thread.sleep(r.nextInt(40)+80);           //Sprawdzanie co 80-120 milisekund
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+
             }
         });
         checking.start();
